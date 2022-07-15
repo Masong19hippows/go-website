3db9c7ff54e109225224b085b0d6415a33ee3deb
Mason Garten <garten323@gmail.com>
2022-07-15 09:17:42 -0500

    Fixing proxy

--- main.go
+++ main.go
@@ -214,9 +214,9 @@ func main() {
 	router.NoMethod(SendError(Response{Status: http.StatusMethodNotAllowed, Error: []string{"File Not Found on Server"}}))
 	router.NoRoute(SendError(Response{Status: http.StatusNotFound, Error: []string{"File Not Found on Server"}}))
 	router.Any("/octo", proxy)
-	router.Any("/octo/:test", proxy)
-	router.Any("/octo/:test/:first", proxy)
-	router.Any("/octo/:test/:first/:second/:third/:fourth/:fith", proxy)
+	router.Any("/octo/:first", proxy)
+	router.Any("/octo/:first/:second", proxy)
+	// router.Any("/octo/:first/:second/:third/:fourth/:fith", proxy)
 
 	router.StaticFile("/", "assets/index.html")
 	router.POST("/send_email", sendEmail(*password))
