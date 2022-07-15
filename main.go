fc750029552400c7d876d7fe7abc8f2645b23033
Mason Garten <garten323@gmail.com>
2022-07-15 13:50:37 -0500

    Fixing proxy

--- main.go
+++ main.go
@@ -191,14 +191,14 @@ func proxy(c *gin.Context) {
 		body := ioutil.NopCloser(bytes.NewReader(b))
 		resp.Body = body
 
-		// location, err := resp.Location()
-		// if err == nil {
-		// 	newLocation := location.String()
-		// 	newLocation = strings.Replace(newLocation, "http://192.168.1.157:80/login/", "http://localhost:8081"+"/octo/login", -1)
-		// 	newLocation = strings.Replace(newLocation, "http://192.168.1.157/login/", "http://localhost:8081"+"/octo/login", -1)
-		// 	resp.Header.Set("location", newLocation)
-		// 	log.Printf("Response is redirecting from %v and now to %v", location, newLocation)
-		// }
+		location, err := resp.Location()
+		if err == nil {
+			newLocation := location.String()
+			newLocation = strings.Replace(newLocation, "http://192.168.1.157:80/login/", "http://localhost:8081"+"/octo/login/", -1)
+			newLocation = strings.Replace(newLocation, "http://192.168.1.157/login/", "http://localhost:8081"+"/octo/login/", -1)
+			resp.Header.Set("location", newLocation)
+			log.Printf("Response is redirecting from %v and now to %v", location, newLocation)
+		}
 		resp.ContentLength = int64(len(b))
 		resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
 		return nil
