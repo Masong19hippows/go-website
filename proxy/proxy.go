4192dd04ebde67ba8334174645f1d1bf321ba68d
masong19hippows <garten323@gmail.com>
2022-10-20 13:21:24 -0500

    Fixinf proxy private ip

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -173,7 +173,7 @@ func lookProxy(lookup Proxy, c *gin.Context) {
 
 	//Modify the response so that links/redirects work
 	proxy.ModifyResponse = func(resp *http.Response) (err error) {
-		log.Printf("&v", resp.request.RemoteAddr)
+		log.Printf("&v", resp.Request.RemoteAddr)
 		//Correcting The response body so that href links work
 		b, err := ioutil.ReadAll(resp.Body) //Read html
 		if err != nil {
