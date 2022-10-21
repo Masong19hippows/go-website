e065d5134f3d1f2b40237b0b3845652da3c221d9
masong19hippows <garten323@gmail.com>
2022-10-21 14:30:07 -0500

    Fixing https support

--- main.go
+++ main.go
@@ -7,9 +7,8 @@ import (
 	"strconv"
 	"os"
     "path/filepath"
-	"crypto/tls"
 
-	"golang.org/x/crypto/acme/autocert"
+	"github.com/caddyserver/certmagic"
 	"github.com/gin-gonic/gin"
 	"github.com/masong19hippows/go-website/email"
 	"github.com/masong19hippows/go-website/proxy"
@@ -23,11 +22,8 @@ func main() {
     exPath := filepath.Dir(ex)
 
 	//get port flag and password flag
-	portHTTP := flag.Int("portHTTP", 80, "Select the port that you wish the http server to run on")
-	portHTTPS := flag.Int("portHTTPS", 443, "Select the port that you wish the https server to run on")
 	password := flag.String("password", "", "Choose the app password obtained form no-reply email account")
 	flag.Parse()
-	log.Println("Using port", *portHTTP, "For http webserver and port", *portHTTPS, "for https server")
 
 	// non-verbose
 	gin.SetMode(gin.ReleaseMode)
@@ -41,30 +37,9 @@ func main() {
 	router.StaticFile("/index.css", exPath + "/assets/index.css")
 	router.StaticFS("/images", http.Dir(exPath + "/assets/images/"))
 
-	ch := make(chan error)
-	go func (ch chan error) {
-		err := router.Run("0.0.0.0:" + strconv.Itoa(*portHTTP))
-		ch <- err
-		return
-	}(ch)
-	go func (ch chan error) {
-		certManager := autocert.Manager{
-			Prompt: autocert.AcceptTOS,
-			HostPolicy: autocert.HostWhitelist("masongarten.sytes.net"),
-			Cache:  autocert.DirCache(exPath + "/certs"),
-		}
-	
-		server := &http.Server{
-			Addr:    ":443",
-			Handler: router,
-			TLSConfig: &tls.Config{
-				GetCertificate: certManager.GetCertificate,
-			},
-		}
-		err := server.ListenAndServeTLS("", "")
-		ch <- err
-		return
-	}(ch)
+	err = certmagic.HTTPS([]string{"masongarten.sytes.net"}, router)
+	if err != nil{
+		panic(err)
+	}
 
-	panic(<-ch)
 }
