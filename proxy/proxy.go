5f8a4257f7f533f294af601d86dd076d9d3aca50
masong19hippows <garten323@gmail.com>
2022-10-21 18:25:24 -0500

    i hate everything

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -153,6 +153,7 @@ func lookProxy(lookup Proxy, c *gin.Context) {
 
 		//Making sure https goies through the server's https
 		if c.Request.TLS == nil && remote.Scheme == "https"{
+			log.Println("https" + c.Request.URL.Host + c.Request.URL.Path + c.Request.URL.RawQuery)
 			c.Redirect(http.StatusMovedPermanently, "https" + c.Request.URL.Host + c.Request.URL.Path + c.Request.URL.RawQuery)
 			return
 		}
