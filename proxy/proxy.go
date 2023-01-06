b5bdb7b7af9bf950d62dad9f76addbfeab7cb4c0
Mason Garten <garten323@gmail.com>
2023-01-06 15:47:45 -0600

    test

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -114,7 +114,7 @@ func Handler(c *gin.Context) {
 				if err != nil {
 					log.Println(err)
 					continue
-				} else if resp.StatusCode == 404 || resp.StatusCode == 400 {
+				} else if resp.StatusCode != 200 {
 					if i == len(Proxies)-1 {
 						cat.SendError(cat.Response{Status: http.StatusNotFound, Error: []string{"File Not Found on Server"}}, c)
 						return
