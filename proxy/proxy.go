f3616d38e94f40a5c640ad58b11ae05015ae6d93
Mason Garten <garten323@gmail.com>
2023-04-17 18:01:05 -0500

    Update proxy.go

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -251,6 +251,7 @@ func lookProxy(lookup Proxy, c *gin.Context) {
 		resp.ContentLength = int64(len(b))
 		resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
 		resp.Header.Set("Content-Type", c.Writer.Header().Get("Content-Type"))
+		log.Println(c.Writer.Header().Get("Content-Type"))
 		return nil
 	}
 
