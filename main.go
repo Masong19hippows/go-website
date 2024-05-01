d5f7fa75814c136501f86dbb98f7951227de3054
Mason Garten <garten323@gmail.com>
2024-05-01 18:08:43 -0500

    Making Proxy Optional

--- main.go
+++ main.go
@@ -21,14 +21,19 @@ func main() {
 	exPath := filepath.Dir(ex)
 
 	//get port flag and password flag
-	password := flag.String("password", "", "Choose the app password obtained form no-reply email account")
+	password := flag.String("password", "", "Choose the app password obtained form no-reply email account.")
+	httpPort := flag.String("HTTPPort", "80", "Choose the HTTP Port. Default is 80.")
+	proxyEnable := flag.Bool("proxy", true, "Enable to disable proxy. Enabled by Defauly.")
+	flag.Parse() 
 
 	// non-verbose
 	gin.SetMode(gin.ReleaseMode)
 
 	//default routes + the proxy handler
 	router := gin.New()
-	router.Use(proxy.Handler)
+	if *proxyEnable {
+		router.Use(proxy.Handler)
+	}
 	router.StaticFile("/", exPath+"/assets/index.html")
 	router.POST("/send_email", email.SendEmail(*password))
 	router.StaticFile("/favicon.ico", exPath+"/assets/favicon.ico")
@@ -37,29 +42,33 @@ func main() {
 
 	ch := make(chan error)
 	go func(ch chan error) {
-		err := router.Run("0.0.0.0:80")
+		err := router.Run("0.0.0.0:" + *httpPort)
 		ch <- err
 	}(ch)
-	go func(ch chan error) {
 
-		var list []string
-		var proxies []proxy.Proxy
-		proxy.GetProxies(&proxies)
-		list = append(list, "masongarten.com")
-		for _, proxy := range proxies {
-			if proxy.Hostname == true {
-				list = append(list, proxy.AccessPrefix + ".masongarten.com")
+	if *proxyEnable {
+		go func(ch chan error) {
+
+			var list []string
+			var proxies []proxy.Proxy
+			proxy.GetProxies(&proxies)
+			list = append(list, "masongarten.com")
+			for _, proxy := range proxies {
+				if proxy.Hostname {
+					list = append(list, proxy.AccessPrefix + ".masongarten.com")
+				}
 			}
-		}
-		
-		m := autocert.Manager{
-			Prompt:     autocert.AcceptTOS,
-			HostPolicy: autocert.HostWhitelist(list...),
-			Cache:      autocert.DirCache(exPath + "/certs"),
-		}
-		err := autotls.RunWithManager(router, &m)
-		ch <- err
-	}(ch)
-
+			
+			m := autocert.Manager{
+				Prompt:     autocert.AcceptTOS,
+				HostPolicy: autocert.HostWhitelist(list...),
+				Cache:      autocert.DirCache(exPath + "/certs"),
+			}
+			err := autotls.RunWithManager(router, &m)
+			ch <- err
+		}(ch)
+	
+	}
+	
 	panic(<-ch)
 }
